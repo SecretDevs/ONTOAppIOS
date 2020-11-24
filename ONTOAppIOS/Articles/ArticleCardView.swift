eb17a0c32a83c47baa5548fc97c2b59f44108d67
BigBey <bey.sviatoslav@yandex.ru>
2020-11-24 04:16:04 -0800

    Merge articles with tab bar in develop.

--- ONTOAppIOS/Articles/ArticleCardView.swift
+++ ONTOAppIOS/Articles/ArticleCardView.swift
@@ -1,9 +1,6 @@
 //
-//  VCardView.swift
-//  ONTOAppIOS
-//
-//  Created by Ivanov Ivan on 15.11.2020.
-//  Copyright © 2020 Ivanov Ivan. All rights reserved.
+// Created by Ivanov Ivan on 24.11.2020.
+// Copyright (c) 2020 ___FULLUSERNAME___. All rights reserved.
 //
 
 import Foundation
@@ -11,27 +8,27 @@ import SwiftUI
 import URLImage
 
 struct ArticleCardView: View {
-   let text: String
-   let url: URL
-   
-   var body: some View {
-            VStack{
-                Spacer()
-                Text(self.text)
-                        .font(.system(size: 16, weight: .medium))
-                        .foregroundColor(Color.white)
-                        .padding(.all, 10)
-                        .layoutPriority(.greatestFiniteMagnitude)
+    let text: String
+    let url: URL
 
-            }
-                    .frame(maxWidth: UIScreen.main.bounds.width, alignment: .bottom)
-                    .frame(height: 200)
-                    .background(Color.black.opacity(0.5))
-                    .background(URLImage(self.url, content: {image in
-                        image.image
-                                .centerCropped()
-                    }))
-                    .cornerRadius(15)
-                    .padding(.all, 5)
-   }
+    var body: some View {
+        VStack{
+            Spacer()
+            Text(self.text)
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(Color.white)
+                    .padding(.all, 10)
+                    .layoutPriority(.greatestFiniteMagnitude)
+
+        }
+                .frame(maxWidth: UIScreen.main.bounds.width, alignment: .bottom)
+                .frame(height: 200)
+                .background(Color.black.opacity(0.5))
+                .background(URLImage(self.url, content: {image in
+                    image.image
+                            .centerCropped()
+                }))
+                .cornerRadius(15)
+                .padding(.all, 5)
+    }
 }
